cc1213b05c87d9006e43c4027666cac939273996
Muhammad Fazhil <mfazhil.02@gmail.com>
2021-04-17 22:03:03 +0700

    feat: :sparkles: Penambahan tabel

--- data_ruangan.php
+++ data_ruangan.php
@@ -4,17 +4,34 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./vendors/normalize/normalize.css" />
-  <link rel="stylesheet" href="./styles/style.css" />
+  <link rel="stylesheet" href="./styles/main.css" />
   <script src="./vendors/jquery/jquery.js"></script>
-  <title>SIPR | Data Ruangan</title>
+  <title>Data Ruangan | SIPR</title>
 </head>
 
 <body>
   <?php require "includes/navbar.php"; ?>
 
-  <main class="hero container">
-
+  <main class="room">
+    <h1>Data Ruangan</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Ruangan</th>
+          <th>Jenis Ruangan</th>
+          <th>Prosedur</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <td>ruangan 1</td>
+          <td>jenis b</td>
+          <td>Prosedur A</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 
   <?php require "includes/footer.php"; ?>
--- penilaian.php
+++ penilaian.php
@@ -4,17 +4,36 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./vendors/normalize/normalize.css" />
-  <link rel="stylesheet" href="./styles/style.css" />
+  <link rel="stylesheet" href="./styles/main.css" />
   <script src="./vendors/jquery/jquery.js"></script>
-  <title>SIPR | Penilaian</title>
+  <title>Penilaian | SIPR</title>
 </head>
 
 <body>
   <?php require "includes/navbar.php"; ?>
 
-  <main class="">
-
+  <main class="valuation">
+    <h1>Penilaian</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Tanggal</th>
+          <th>Petugas</th>
+          <th>Ruangan</th>
+          <th>Nilai</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <td>23-01-01</td>
+          <td>petgugasas</td>
+          <td>ruangan 1</td>
+          <td>100</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 
   <?php require "includes/footer.php"; ?>
