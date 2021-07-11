cf17c4b04310441f79fddf8cfd77a47fc643ba95
Muhammad Fazhil <mfazhil.02@gmail.com>
2021-07-11 11:51:55 +0700

    feat: :poop: Penambahan algoritma fuzzy

--- pengecekan.php
+++ pengecekan.php
@@ -14,7 +14,25 @@ require_once __DIR__ . "/_includes/utils.php";
 
 session_start();
 
+function array_cartesian()
+{
+  $_ = func_get_args();
+  if (count($_) == 0)
+    return array();
+  $a = array_shift($_);
+  if (count($_) == 0)
+    $c = array(array());
+  else
+    $c = call_user_func_array(__FUNCTION__, $_);
+  $r = array();
+  foreach ($a as $v)
+    foreach ($c as $p)
+      $r[] = array_merge(array($v), $p);
+  return $r;
+}
+
 $is_user = count($_SESSION) > 0 && $_SESSION["role"] === Role::USER;
+$is_admin = count($_SESSION) > 0 && $_SESSION["role"] === Role::ADMIN;
 
 if ($is_user && $_SERVER["REQUEST_METHOD"] === "POST") {
   try {
@@ -40,6 +58,8 @@ while ($ruang = $data_ruangan->fetch()) {
   $id_ruang = $ruang["IdRuang"];
   $daftar_tanggal = $db->query("SELECT TglPengecekan FROM pengecekan LEFT JOIN pruang ON pengecekan.IdPRuang = pruang.Iddia LEFT JOIN ruang ON pruang.idruang = ruang.IdRuang WHERE ruang.IdRuang = $id_ruang GROUP BY pengecekan.TglPengecekan");
   if ($daftar_tanggal->rowCount() === 0) continue;
+  $data_rule = $db->query("SELECT * FROM rule WHERE IdRuang = $id_ruang");
+  $rules = $data_rule->fetchAll();
   while ($tanggal = $daftar_tanggal->fetch()) {
     $date = $tanggal["TglPengecekan"];
     $daftar_petugas = $db->query("SELECT pengecekan.idPetugas, NamaPetugas FROM pengecekan LEFT JOIN pruang ON pengecekan.IdPRuang = pruang.Iddia LEFT JOIN petugas ON pengecekan.idPetugas = petugas.IdPetugas WHERE pruang.idruang = $id_ruang AND TglPengecekan = '$date' GROUP BY NamaPetugas");
@@ -48,16 +68,88 @@ while ($ruang = $data_ruangan->fetch()) {
       $list_prosedur = array();
       $list_nilai = array();
       $list_id = array();
-      $first = true;
+      $list_fuzzifikasi = array();
+      $list_status = array();
+      $list_id_himpunan = array();
       $id_petugas = $petugas["idPetugas"];
-      $data_pengecekan = $db->query("SELECT idPengecekan, Nilai, NamaProsedur FROM pengecekan LEFT JOIN pruang ON pengecekan.IdPRuang = pruang.Iddia LEFT JOIN prosedur ON pruang.idprosedur = prosedur.IdProsedur WHERE pruang.idruang = $id_ruang AND TglPengecekan = '$date' AND idPetugas = $id_petugas");
+      $data_pengecekan = $db->query("SELECT idPengecekan, Nilai, NamaProsedur, prosedur.IdProsedur FROM pengecekan LEFT JOIN pruang ON pengecekan.IdPRuang = pruang.Iddia LEFT JOIN prosedur ON pruang.idprosedur = prosedur.IdProsedur WHERE pruang.idruang = $id_ruang AND TglPengecekan = '$date' AND idPetugas = $id_petugas");
       if ($data_pengecekan->rowCount() === 0) continue;
       while ($pengecekan = $data_pengecekan->fetch()) {
         array_push($list_prosedur, $pengecekan["NamaProsedur"]);
         array_push($list_nilai, $pengecekan["Nilai"]);
         array_push($list_id, $pengecekan["idPengecekan"]);
+
+        $keanggotaan = [];
+        $fuzzifikasi = [];
+        $id_prosedur = $pengecekan["IdProsedur"];
+        $arr_id_himpunan = array();
+
+        $data_himpunan = $db->query("SELECT * FROM himpunan WHERE IdProsedur = $id_prosedur");
+        if ($data_himpunan->rowCount() === 0) continue;
+        $jumlah_himpunan = $data_himpunan->rowCount();
+        $urutan = 1;
+        while ($himpunan = $data_himpunan->fetch()) {
+          if ($himpunan["Bawah"] <= $pengecekan["Nilai"] && $pengecekan["Nilai"] <= $himpunan["Atas"]) {
+            array_push($keanggotaan, array_merge($himpunan, ["urutan" => $urutan]));
+          }
+          array_push($arr_id_himpunan, $himpunan["IdHimpunan"]);
+          $urutan++;
+        }
+        array_push($list_id_himpunan, $arr_id_himpunan);
+
+        foreach ($keanggotaan as $fungsi) {
+          $id_himpunan = $fungsi["IdHimpunan"];
+          if ($pengecekan["Nilai"] <= $fungsi["Bawah"]) {
+            if ($fungsi["urutan"] === 1) {
+              $fuzzifikasi["$id_himpunan"] = 1;
+            } else {
+              $fuzzifikasi["$id_himpunan"] = 0;
+            }
+            continue;
+          }
+          if ($fungsi["Atas"] <= $pengecekan["Nilai"]) {
+            if ($fungsi["urutan"] === $jumlah_himpunan) {
+              $fuzzifikasi["$id_himpunan"] = 1;
+            } else {
+              $fuzzifikasi["$id_himpunan"] = 0;
+            }
+            continue;
+          }
+          if (!empty($fungsi["Tengah"])) {
+            if ($fungsi["Bawah"] < $pengecekan["Nilai"] && $pengecekan["Nilai"] < $fungsi["Tengah"]) {
+              $fuzzifikasi["$id_himpunan"] = ($pengecekan["Nilai"] - $fungsi["Bawah"]) / ($fungsi["Tengah"] - $fungsi["Bawah"]);
+            } else if ($fungsi["Tengah"] < $pengecekan["Nilai"] && $pengecekan["Nilai"] < $fungsi["Atas"]) {
+              $fuzzifikasi["$id_himpunan"] = ($fungsi["Atas"] - $pengecekan["Nilai"]) / ($fungsi["Atas"] - $fungsi["Tengah"]);
+            }
+            continue;
+          } else {
+            if ($fungsi["urutan"] === $jumlah_himpunan) {
+              $fuzzifikasi["$id_himpunan"] = ($pengecekan["Nilai"] - $fungsi["Bawah"]) / ($fungsi["Atas"] - $fungsi["Bawah"]);
+            } else if ($fungsi["urutan"] === 1) {
+              $fuzzifikasi["$id_himpunan"] = ($fungsi["Atas"] - $pengecekan["Nilai"]) / ($fungsi["Atas"] - $fungsi["Bawah"]);
+            }
+            continue;
+          }
+        }
+        $nilai_max =  max($fuzzifikasi);
+        $list_fuzzifikasi[array_search($nilai_max, $fuzzifikasi)] = $nilai_max;
       }
-      array_push($data, ["id_ruang" => $ruang["IdRuang"], "tanggal" => $tanggal["TglPengecekan"], "nama_ruang" => $ruang["NamaRuang"], "petugas" => $petugas["NamaPetugas"], "prosedur" => $list_prosedur, "nilai" => $list_nilai, "ids" => $list_id]);
+      $id_rule = array_cartesian(...$list_id_himpunan);
+      $key_fuzzifikasi = array();
+      foreach ($list_fuzzifikasi as $key => $value) {
+        array_push($key_fuzzifikasi, $key);
+      }
+      $index_rule = array_search($key_fuzzifikasi, $id_rule);
+      if ($rules !== false) {
+        if (count($rules) !== count($id_rule)) {
+          $status = "Jumlah rule tidak cocok";
+        } else {
+          $status = !empty($rules[$index_rule]) ? $rules[$index_rule]["Rule"] : "Tidak ada rule yg cocok";
+        }
+      } else {
+        $status = "Rule tidak ada";
+      }
+      array_push($data, ["id_ruang" => $ruang["IdRuang"], "tanggal" => $tanggal["TglPengecekan"], "nama_ruang" => $ruang["NamaRuang"], "petugas" => $petugas["NamaPetugas"], "prosedur" => $list_prosedur, "nilai" => $list_nilai, "ids" => $list_id, "status" => $status]);
     }
   }
 }
@@ -96,7 +188,8 @@ usort($data, "sort_date");
           <th>Ruangan</th>
           <th>Prosedur</th>
           <th>Nilai</th>
-          <?php if ($is_user) { ?>
+          <th>Status</th>
+          <?php if ($is_user || $is_admin) { ?>
             <th class="table__action">Aksi</th>
           <?php } ?>
         </tr>
@@ -119,6 +212,7 @@ usort($data, "sort_date");
                 echo $nilai . '<br>';
               } ?>
             </td>
+            <td><?= $data_cek["status"]; ?></td>
             <?php if ($is_user) { ?>
               <td>
                 <form class="table__cta" method="POST">
@@ -131,6 +225,12 @@ usort($data, "sort_date");
                 </form>
               </td>
             <?php } ?>
+            <?php if ($is_admin) { ?>
+              <td style="text-align: center;">
+                <a href="./fuzzy.php?id[]=<?= join("&id[]=", $data_cek["ids"]); ?>" class="button--blue small">Lihat Perhitungan</a>
+              </td>
+            <?php } ?>
+
           </tr>
         <?php } ?>
 
