eb27315c9e565cdfe4c191fe459a9fe3e2254dd2
Muhammad Fazhil <mfazhil.02@gmail.com>
2021-07-11 11:53:29 +0700

    refactor: :recycle: Penyesuaian class untuk halaman fuzzy dan rule

--- _includes/navbar.php
+++ _includes/navbar.php
@@ -39,7 +39,7 @@ function addActiveClass(String $pageName): String
           </a>
         </li>
         <li class="nav__item">
-          <a href="./pengecekan.php" class="nav__link <?= addActiveClass("pengecekan") ?>">
+          <a href="./pengecekan.php" class="nav__link <?= addActiveClass("pengecekan") . addActiveClass("fuzzy"); ?>">
             <svg class="nav__icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
             </svg>
@@ -47,7 +47,7 @@ function addActiveClass(String $pageName): String
           </a>
         </li>
         <li class="nav__item">
-          <a href="./data-ruangan.php" class="nav__link <?= addActiveClass("data-ruangan") ?>">
+          <a href="./data-ruangan.php" class="nav__link <?= addActiveClass("data-ruangan") . addActiveClass("rule"); ?>">
             <svg class="nav__icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
             </svg>
