d7ef4af4d2be96e3a4993a98a66ae03a67947158
Muhammad Fazhil <mfazhil.02@gmail.com>
2021-07-22 03:39:54 +0700

    feat: :sparkles: Penambahan output nilai max dan rule

--- fuzzy.php
+++ fuzzy.php
@@ -40,6 +40,8 @@ $list_id = $_GET["id"];
 
 $data_perhitungan = array();
 $list_id_himpunan = array();
+$list_id_himpunan_max = array();
+$array_id_ruang = array();
 foreach ($list_id as $id_pengecekan) {
   $list_prosedur = array();
   $list_nilai = array();
@@ -48,7 +50,7 @@ foreach ($list_id as $id_pengecekan) {
   if ($data_pengecekan->rowCount() === 0) continue;
   $pengecekan = $data_pengecekan->fetch();
   $id_prosedur = $pengecekan["IdProsedur"];
-
+  array_push($array_id_ruang, $pengecekan["idruang"]);
   $data_himpunan = $db->query("SELECT * FROM himpunan WHERE IdProsedur = $id_prosedur");
   if ($data_himpunan->rowCount() === 0) continue;
   $jumlah_himpunan = $data_himpunan->rowCount();
@@ -67,11 +69,7 @@ foreach ($list_id as $id_pengecekan) {
 
   $urutan = 1;
   while ($himpunan = $data_himpunan->fetch()) {
-    if ($himpunan["Bawah"] <= $pengecekan["Nilai"] && $pengecekan["Nilai"] <= $himpunan["Atas"]) {
-      array_push($keanggotaan, array_merge($himpunan, ["urutan" => $urutan]));
-    } else {
-      $himpun[$urutan - 1] = array_merge($himpunan, ["Nilai" => 0]);
-    }
+    array_push($keanggotaan, array_merge($himpunan, ["urutan" => $urutan]));
     array_push($arr_id_himpunan, $himpunan["IdHimpunan"]);
     $urutan++;
   }
@@ -119,10 +117,57 @@ foreach ($list_id as $id_pengecekan) {
       continue;
     }
   }
-
-  array_push($data_perhitungan, ["NamaProsedur" => $pengecekan["NamaProsedur"], "Nilai" => $pengecekan["Nilai"], "fuzzy" => $himpun]);
+  $array_nilai = [];
+  foreach ($himpun as $key => $isi_himpunan) {
+    $array_nilai[$key] = $isi_himpunan["Nilai"];
+  }
+  $nilai_max =  max($array_nilai);
+  $max = $himpun[array_search($nilai_max, $array_nilai)];
+  array_push($list_id_himpunan_max, $max["IdHimpunan"]);
+  array_push($data_perhitungan, ["NamaProsedur" => $pengecekan["NamaProsedur"], "Nilai" => $pengecekan["Nilai"], "fuzzy" => $himpun, "max" => $max]);
 }
-
+$id_rule = array_cartesian(...$list_id_himpunan);
+$list_id_ruang = array_unique($array_id_ruang);
+if (count($list_id_ruang) > 1) throw new Error("Ruang lebih dari satu");
+$id_ruang = $list_id_ruang[0];
+$data_rule = $db->query("SELECT * FROM rule WHERE IdRuang = $id_ruang");
+$rules = $data_rule->fetchAll();
+$index_rule = array_search($list_id_himpunan_max, $id_rule);
+if ($rules !== false) {
+  if (count($rules) !== count($id_rule)) {
+    $status = "Jumlah rule tidak cocok";
+  } else {
+    $status = !empty($rules[$index_rule]) ? $rules[$index_rule]["Rule"] : "Tidak ada rule yg cocok";
+  }
+} else {
+  $status = "Rule tidak ada";
+}
+if (!empty($rules[$index_rule])) {
+  $data_pruang = $db->query("SELECT * FROM pruang INNER JOIN prosedur ON pruang.idprosedur = prosedur.IdProsedur WHERE idruang = $id_ruang");
+  $list_prosedur = [];
+  $list_himpunan = [];
+  while ($pruang = $data_pruang->fetch()) {
+    array_push($list_prosedur, $pruang["NamaProsedur"]);
+    $id_prosedur = $pruang["IdProsedur"];
+    $data_himpunan = $db->query("SELECT * FROM himpunan WHERE IdProsedur = $id_prosedur");
+    $himpunans = [];
+    while ($himpunan = $data_himpunan->fetch()) {
+      array_push($himpunans, $himpunan["NamaHimpunan"]);
+    }
+    array_push($list_himpunan, $himpunans);
+  }
+  $rule = array_cartesian(...$list_himpunan);
+  $text = "Jika ";
+  foreach ($rule[$index_rule] as $key => $value) {
+    $prosedur = $list_prosedur[$key];
+    $text = $text . "$prosedur $value";
+    if ($key !== count($rule[$index_rule]) - 1) {
+      $text = $text . " dan ";
+    } else {
+      $text = $text . " maka ruang:";
+    }
+  }
+}
 ?>
 
 <body>
@@ -150,7 +195,7 @@ foreach ($list_id as $id_pengecekan) {
           <tr>
             <th>Nilai</th>
             <th style="padding-left: 1rem;">Nama Himpunan</th>
-            <th>Fuzzifikasi</th>
+            <th style="border-right-width: 1px;">Fuzzifikasi</th>
           </tr>
         </thead>
         <tbody>
@@ -165,14 +210,27 @@ foreach ($list_id as $id_pengecekan) {
               }
               ?>
               <td style="padding-left: 1rem;"><?= $fuzzy["NamaHimpunan"] ?></td>
-              <td><?= $fuzzy["Nilai"] ?></td>
+              <td style="border-right-width: 1px;"><?= $fuzzy["Nilai"] ?></td>
             </tr>
           <?php } ?>
         </tbody>
-
-
+        <tfoot>
+          <tr style="background-color: #e5e7eb; border: 1px solid #d1d5db; height: 2.5rem;">
+            <th style="border-right: 1px solid #d1d5db;">Max</th>
+            <th style="text-align: start; padding-left: 1rem;"><?= $data["max"]["NamaHimpunan"]; ?></th>
+            <th style="text-align: start; border-right: 1px solid #d1d5db;"><?= $data["max"]["Nilai"]; ?></th>
+          </tr>
+        </tfoot>
       </table>
+
     <?php } ?>
+    <h1>Rule</h1>
+    <?php if (!empty($rules[$index_rule])) { ?>
+      <div><?= $text . " " . $status; ?></div>
+    <?php } else { ?>
+      <div><?= $status; ?></div>
+    <?php } ?>
+
   </main>
 
   <?php include __DIR__ . "/_includes/footer.php"; ?>
